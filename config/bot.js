fc0c258cf243c684a0e3820fed629d1438e1c737
Kaliboy002 <146410941+Kaliboy002@users.noreply.github.com>
2024-11-29 11:16:12 +0430

    Update bot.js

--- config/bot.js
+++ config/bot.js
@@ -3,6 +3,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 const node_telegram_bot_api_1 = __importDefault(require("node-telegram-bot-api"));
-const token = process.env.TELEGRAM_TOKEN;
+const token = process.env.7964758764:AAGuYr8voir-lrOBUim_mj1w2KfY1-7rsgI;
 const bot = new node_telegram_bot_api_1.default(token, { polling: true });
 module.exports = bot;
